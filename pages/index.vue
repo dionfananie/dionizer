f00631fe39ad813aa499aaa324bb11f95e58877e
dionfananie <dionfananie@gmail.com>
2019-07-18 13:48:44 +0700

    pages works

--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,9 @@
         Automated Deployment Nuxt App using Third Parties
       </h2>
       <div class="wrapper--button">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-16 rounded-full">
+        <nuxt-link to="/works" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-16 rounded-full">
           Button
-        </button>
+        </nuxt-link>
       </div>
     </div>
   </div>
---
+++ pages/works.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container mx-auto">
+        <div class="my-10">
+            <p class="text-5xl text-center">Works</p>
+        </div>
+        <div class="flex mb-4">
+            <div class="w-1/3" v-for="i in 3" :key="i">
+                <Card/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+const Card = () => import('~/components/Card');
+export default {
+    components:{
+        Card
+    }
+
+}
+</script>
+
+<style>
+
+</style>
