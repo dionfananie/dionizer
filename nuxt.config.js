cceed75e752552ff82456f66e7d5e28fd369b188
dionfananie <dionfananie@gmail.com>
2019-07-18 16:52:20 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,14 @@
 
+
+const {headConfig} = require('./config/head.js');
+// import {headConfig} from './config/head.js';
+
 module.exports = {
   mode: 'universal',
   /*
   ** Headers of the page
   */
-  head: {
-    title: "Dionizer",
-    meta: [
-      { charset: "utf-8" },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1"
-      },
-      { hid: "description", name: "description", content: "Automated Deployment Nuxt Apps using Third Parties" },
-      { hid: "keyword", name: "keyword", content: "Automated, Deployment, Nuxt, Apps using, Third Parties, CI, CD, Continuous" },
-      { hid: "robot", name: "robot", content: "Index, Follow" },
-      { hid: "og-description", property: "og:description", content: "Automated Deployment Nuxt Apps using Third Parties" },
-      { hid: "og-title", property: "og:title", content: "Dionizer.com" },
-      { hid: "og-type", property: "og:type", content: "website" },
-      { hid: "og-url", property: "og:url", content: "https://www.dionizer-netlify.com" },
-      { hid: 'og-image-alt', property: 'og:image:alt', content: "Automated Deployment Nuxt Apps using Third Parties" },
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "dionizer.ico" },
-    ],
-
-  },
+  head: [],
   /*
   ** Customize the progress-bar color
   */
@@ -40,7 +23,6 @@ module.exports = {
   icon: {
     iconSrc: './static/logo/dionizer.png'
   },
-
 
   manifest: {
     name: 'Rentfix.com',
