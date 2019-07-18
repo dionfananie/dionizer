ce9607ee7a778e6f786527fa31afccb1d03932dc
dionfananie <dionfananie@gmail.com>
2019-07-18 10:32:53 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,12 @@
     <div>
       <logo />
       <h1 class="title">
-        dionizer
+        Dionizer
       </h1>
       <h2 class="subtitle">
         Automated Deployment Nuxt App using Third Parties
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+     
     </div>
   </div>
 </template>
