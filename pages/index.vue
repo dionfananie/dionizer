154bf5fd05209fd60fb3cbedc61688edcae64581
dionfananie <dionfananie@gmail.com>
2019-07-18 10:46:05 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,11 @@
       <h2 class="subtitle">
         Automated Deployment Nuxt App using Third Parties
       </h2>
-     
+      <div class="wrapper--button">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          Button
+        </button>
+      </div>
     </div>
   </div>
 </template>
