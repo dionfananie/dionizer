62865fd3957bb871bbdaf8dd31a5e31694452de3
Dion Edo Fananie <dionfananie@gmail.com>
2020-06-01 14:19:33 +0700

    add core-js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.vue"],
   theme: {
     extend: {}
   },
   variants: {}
-}
+};
